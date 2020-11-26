95eadfabff53b8aafd297d53935976164319b03f
admcollect <73002906+admcollect@users.noreply.github.com>
2020-11-25 21:22:12 -0600

    Actualizacion comprobante

--- ecollect/Core/Observer/SendMailOnOrderSuccess.php
+++ ecollect/Core/Observer/SendMailOnOrderSuccess.php
@@ -1,56 +1,61 @@
 <?php
- 
 namespace ecollect\Core\Observer;
- 
+
+use Magento\Framework\Event\Observer as EventObserver;
 use Magento\Framework\Event\ObserverInterface;
- 
+
 class SendMailOnOrderSuccess implements ObserverInterface
 {
-    /**
-     * @var \Magento\Sales\Model\OrderFactory
-     */
-    protected $orderModel;
- 
-    /**
-     * @var \Magento\Sales\Model\Order\Email\Sender\OrderSender
-     */
-    protected $orderSender;
- 
-    /**
-     * @var \Magento\Checkout\Model\Session $checkoutSession
-     */
-    protected $checkoutSession;
- 
-    /**
-     * @param \Magento\Sales\Model\OrderFactory $orderModel
-     * @param \Magento\Sales\Model\Order\Email\Sender\OrderSender $orderSender
-     * @param \Magento\Checkout\Model\Session $checkoutSession
-     *
-     * @codeCoverageIgnore
-     */
-    public function __construct(
-        \Magento\Sales\Model\OrderFactory $orderModel,
-        \Magento\Sales\Model\Order\Email\Sender\OrderSender $orderSender,
-        \Magento\Checkout\Model\Session $checkoutSession
-    )
-    {
-        $this->orderModel = $orderModel;
-        $this->orderSender = $orderSender;
-        $this->checkoutSession = $checkoutSession;
-    }
- 
-    /**
-     * @param \Magento\Framework\Event\Observer $observer
-     * @return void
-     */
-    public function execute(\Magento\Framework\Event\Observer $observer)
-    {
-        $orderIds = $observer->getEvent()->getOrderIds();
-        if(count($orderIds))
-        {
-            $this->checkoutSession->setForceOrderMailSentOnSuccess(true);
-            $order = $this->orderModel->create()->load($orderIds[0]);
-            $this->orderSender->send($order, true);
-        }
-    }
+
+	/**
+     * @var \Magento\Sales\Model\OrderFactory
+     * 
+     */
+	protected $orderModel;
+
+	/**
+     * @var \Magento\Sales\Model\Order\Email\Sender\OrderSender
+     * 
+     */
+	protected $orderSender;
+
+	/**
+     * @var \Magento\Checkout\Model\Session $checkoutSession
+     * 
+     */
+	protected $checkoutSession;
+	
+	/**
+     * @param \Magento\Sales\Model\OrderFactory $orderModel
+	 * @param \Magento\Sales\Model\Order\Email\Sender\OrderSender $orderSender
+     * @param \Magento\Checkout\Model\Session $checkoutSession
+	 *
+	 * @codeCoverageIgnore
+     */
+	public function __construct(
+		\Magento\Sales\Model\OrderFactory $orderModel,
+        \Magento\Sales\Model\Order\Email\Sender\OrderSender $orderSender,
+        \Magento\Checkout\Model\Session $checkoutSession
+    )
+	{
+		$this->orderModel = $orderModel;
+        $this->orderSender = $orderSender;
+        $this->checkoutSession = $checkoutSession;
+	}
+
+	/**
+     * @param EventObserver $observer
+     *@return void
+	 *	
+     */
+	public function execute(EventObserver $observer)
+	{
+		$orderIds = $observer->getEvent()->getOrderIds();
+		if(count($orderIds))
+		{
+			$this->checkoutSession->setForceOrderMailSentOnSuccess(true);
+            $order = $this->orderModel->create()->load($orderIds[0]);
+			$this->orderSender->send($order, true);
+		}
+	}
 }
